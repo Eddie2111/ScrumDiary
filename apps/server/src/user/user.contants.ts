cc56e8d85c781ab2330b98be856bb69445304ba4
Eddie2111 <asm.tareq.mahmood@g.bracu.ac.bd>
2024-12-29 00:16:57 +0600

    feat(user): add user repository with crud

---
+++ apps/server/src/user/user.contants.ts
@@ -0,0 +1,4 @@
+export const USER_FOUND = 'User found';
+export const USER_NOT_FOUND = 'User not found';
+export const USER_ALREADY_EXISTS = 'User already exists';
+export const USER_NOT_AUTHORIZED = 'User not authorized';
---
+++ apps/server/test/user.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { USER_FOUND, USER_NOT_FOUND } from 'src/user/user.contants';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/user/:id (GET) gets a user by id succesfully', () => {
+    const TEST_USER_ID = 1;
+    return request(app.getHttpServer())
+      .get(`/user/${TEST_USER_ID}}`)
+      .expect(200)
+      .expect({ statusCode: 200, message: USER_FOUND, data: {id: TEST_USER_ID, name: 'Test User', email: 'test@test.com' } });
+  });
+  it('/user/:id (GET) fails to get a user', () => {
+    const TEST_USER_ID = 0;
+    return request(app.getHttpServer())
+      .get(`/user/${TEST_USER_ID}}`)
+      .expect(200)
+      .expect({ statusCode: 400, message: USER_NOT_FOUND, data: USER_NOT_FOUND });
+  });
+});
