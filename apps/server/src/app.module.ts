ba5788a586311abdea568231de3f8099ed7202fe
ASM Tareq Mahmood <asm.tareq.mahmood@g.bracu.ac.bd>
2024-12-29 00:41:47 +0600

    feat(user): add user repository with crud (#2)

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,9 +3,10 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { AppController } from './app.controller';
 import { TodoModule } from './todo/todo.module';
 import { BoardModule } from './board/board.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TodoModule, BoardModule, MikroOrmModule.forRoot()],
+  imports: [TodoModule, BoardModule, MikroOrmModule.forRoot(), UserModule],
   controllers: [AppController],
   providers: [],
 })
