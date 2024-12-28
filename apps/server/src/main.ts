69d1c3ed311056feb542d9aaf99dc0d24f896eb4
ASM Tareq Mahmood <asm.tareq.mahmood@g.bracu.ac.bd>
2024-12-29 05:00:25 +0600

    feat(user): add user repository (#4)

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,10 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe, VersioningType } from '@nestjs/common';
 
 import { MikroORM } from '@mikro-orm/core';
 import mikroOrmConfig from 'mikro-orm.config';
+import { TransformInterceptor } from './common/interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,9 @@ async function bootstrap() {
   await MikroORM.init(mikroOrmConfig);
   
   app.useGlobalPipes(globalValidationPipeline);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  app.useGlobalInterceptors(new TransformInterceptor());
   app.setGlobalPrefix('api/v1');
 
   app.enableVersioning({
