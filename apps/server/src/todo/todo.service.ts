4d7f3458a08ec9d65ab1f808e1ad7369eb7f571b
Eddie2111 <asm.tareq.mahmood@g.bracu.ac.bd>
2024-12-29 17:21:26 +0600

    feat(todo): add todo repository and integrate controller and service

--- apps/server/src/todo/todo.service.ts
+++ apps/server/src/todo/todo.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { TodoRepository } from './todo.repository';
 
 @Injectable()
 export class TodoService {
+  constructor(private readonly todoRepository: TodoRepository) {}
   create(createTodoDto: CreateTodoDto) {
-    return 'This action adds a new todo';
+    return this.todoRepository.createTask(createTodoDto);
   }
 
-  findAll() {
-    return `This action returns all todo`;
+  findAllByBoard(id: string) {
+    return this.todoRepository.getTasksByBoardId(parseInt(id));
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} todo`;
+    return this.todoRepository.getTaskById(id);
   }
 
   update(id: number, updateTodoDto: UpdateTodoDto) {
-    return `This action updates a #${id} todo`;
+    return this.todoRepository.updateTask(id, updateTodoDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} todo`;
+    return this.todoRepository.deleteTask(id);
   }
 }
